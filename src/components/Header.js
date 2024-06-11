7fb07a32d13d7faf4c0a174d6b7bc4674ec5e26f
Abin M K <abinmkabin@gmail.com>
2024-06-11 23:26:19 +0530

    ADDED netlify

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
-
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
@@ -23,18 +22,22 @@ const Header = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className={`header-container ${scrolled ? 'scrolled' : ''}`}>
-      <Link to="/" className="header-logo">EduConsult</Link>
+      <Link to="/" className="header-logo" onClick={closeMenu}>EduConsult</Link>
       <nav className={`header-nav ${menuOpen ? 'open' : ''}`}>
-        <Link to="/" onClick={toggleMenu}>Home</Link>
-        <Link to="/allotments" onClick={toggleMenu}>Allotments</Link>
-        <Link to="/last-ranks" onClick={toggleMenu}>Last Ranks</Link>
-        <Link to="/fees" onClick={toggleMenu}>Fees</Link>
-        <Link to="/courses" onClick={toggleMenu}>Courses</Link>
-        <Link to="/institutes" onClick={toggleMenu}>Institutes</Link>
-        <Link to="/announcements" onClick={toggleMenu}>Announcements</Link>
-        <Link to="/login" className="header-login" onClick={toggleMenu}>Login</Link>
+        <Link to="/" onClick={closeMenu}>Home</Link>
+        <Link to="/allotments" onClick={closeMenu}>Allotments</Link>
+        <Link to="/last-ranks" onClick={closeMenu}>Last Ranks</Link>
+        <Link to="/fees" onClick={closeMenu}>Fees</Link>
+        <Link to="/courses" onClick={closeMenu}>Courses</Link>
+        <Link to="/institutes" onClick={closeMenu}>Institutes</Link>
+        <Link to="/announcements" onClick={closeMenu}>Announcements</Link>
+        <Link to="/login" className="header-login" onClick={closeMenu}>Login</Link>
       </nav>
       <div className="menu-icon" onClick={toggleMenu}>
         {menuOpen ? <FaTimes /> : <FaBars />}
