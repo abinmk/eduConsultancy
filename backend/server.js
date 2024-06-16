881cbcc128f48af8fee95379d428ffc1889b7505
Abin M K <abinmkabin@gmail.com>
2024-06-17 02:32:34 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,6 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-const cors = require('cors');
-app.use(cors({
-  origin: 'http://rankandseats.s3-website-ap-southeast-2.amazonaws.com'  // Your S3 hosted frontend URL
-}));
-
-
 // MongoDB connection
 const db = process.env.MONGO_URI;
 if (!db) {
@@ -28,23 +18,15 @@ mongoose.connect(db, {
   useUnifiedTopology: true,
 })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1); // Exit the process with an error code
-  });
+  .catch(err => console.error('MongoDB connection error:', err));
 
-// Connection status logging
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose connected to DB Cluster');
-});
-
-mongoose.connection.on('error', (error) => {
-  console.error('Mongoose connection error:', error);
-});
-
-mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose disconnected');
-});
+// Middleware
+app.use(cors({
+  origin: 'http://rankandseats.s3-website-ap-southeast-2.amazonaws.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}));
+app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
